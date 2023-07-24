7e1c84a3f13eb6e7540ed05eea28bc44cf366a93
yusufburakkaradag42 <yusufburakkaradag42@gmail.com>
2023-07-24 16:12:07 +0300

    open modal

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,9 +7,9 @@ const Navbar = ({isNavOpen, setIsNavOpen}) => {
   
  return (
 
-  <div className="flex items-center justify-between  px-3 bg-black py-3 ">
-    <nav>
-      <section className="MOBILE-MENU flex ">
+  <div className="nav flex items-center justify-between  px-3 bg-black py-3  ">
+    <nav> 
+      <section className="MOBILE-MENU flex  ">
         <div
           className="HAMBURGER-ICON space-y-1"
           onClick={() => setIsNavOpen((prev) => !prev)} 
@@ -22,47 +22,45 @@ const Navbar = ({isNavOpen, setIsNavOpen}) => {
         <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"} > 
           <div
             className="CROSS-ICON absolute top-0 right-0 px-8 "
-            onClick={() => setIsNavOpen(false)} // change isNavOpen state to false to close the menu
+            onClick={() => setIsNavOpen(false)} 
           >
-            <svg
-              className="h-8 w-8 text-gray-600"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <line x1="18" y1="6" x2="6" y2="18" />
-              <line x1="6" y1="6" x2="18" y2="18" />
-            </svg>
+  
           </div>
 
-          <ul className="MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px]">
-            <li className="border-b border-gray-400 my-8 uppercase">
-              <a href="/about">About</a>
-            </li>
-            <li className="border-b border-gray-400 my-8 uppercase">
-              <a href="/portfolio">Portfolio</a>
-            </li>
-            <li className="border-b border-gray-400 my-8 uppercase">
-              <a href="/contact">Contact</a>
-            </li>
-          </ul>
+          <div className="main flex">
+           <div className="left">
+            <p> <span class="material-symbols-outlined">public</span> START HERE</p>
+           <a href="/"> <div>
+              <h1>Home and Info</h1>
+              <p>More Information about the project</p>
+            </div></a>
+            <a href="/">
+            <div className="">
+              <h1>Whitepaper</h1>
+              <p>Decentralizing with blackhain</p>
+            </div></a>
+            <a href="/">
+            <div>
+              <h1>Communty</h1>
+              <p>Bulding a theiving communty together</p>
+            </div></a>
+           </div>
+
+           
+           <div className="right">
+            <p><span class="material-symbols-outlined">
+explore
+</span> RESOURCES</p>
+            <div className="right-bg">
+              <div><a href="/">Profile</a> </div>
+              <div><a href="/">Wallet</a> </div>
+              <div><a href="/">Market</a></div>
+            
+            </div>
+           </div>
+          </div>
         </div>
       </section>
-
-      <ul className="DESKTOP-MENU hidden space-x-8  ">
-        <li>
-          <a href="/about">About</a>
-        </li>
-        <li>
-          <a href="/portfolio">Portfolio</a>
-        </li>
-        <li>
-          <a href="/contact">Contact</a>
-        </li>
-      </ul>
     </nav>
     
   </div>
