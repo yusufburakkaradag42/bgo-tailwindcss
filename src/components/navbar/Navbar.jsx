80d03ea0059257b73c8354ab6255b87ea66b166c
yusufburakkaradag42 <yusufburakkaradag42@gmail.com>
2023-07-24 16:27:47 +0300

    modal onclick

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,33 +1,49 @@
-
+import React, { useRef, useEffect } from 'react';
 import "./navbar.css"
 
 
 const Navbar = ({isNavOpen, setIsNavOpen}) => { 
-   
-  
+  const mobileMenuRef = useRef(null);
+
+  useEffect(() => {
+    function handleOutsideClick(event) {
+      // Check if the clicked element is outside the mobile menu
+      if (mobileMenuRef.current && !mobileMenuRef.current.contains(event.target)) {
+        setIsNavOpen(false); // Close the menu
+      }
+    }
+
+    // Attach the event listener when the component mounts
+    document.addEventListener('click', handleOutsideClick);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  }, [setIsNavOpen]);
  return (
 
   <div className="nav flex items-center justify-between  px-3 bg-black py-3  ">
     <nav> 
       <section className="MOBILE-MENU flex  ">
-        <div
-          className="HAMBURGER-ICON space-y-1"
-          onClick={() => setIsNavOpen((prev) => !prev)} 
-        >
+      <div
+            className="HAMBURGER-ICON space-y-1"
+            onClick={(event) => {
+              event.stopPropagation(); 
+              setIsNavOpen((prev) => !prev);
+            }}
+          >
           <span className="block h-0.5 w-8  bg-white"></span>
           <span className="block h-0.5 w-8  bg-white"></span>
           <span className="block h-0.5 w-8  bg-white"></span>
         </div>
 
-        <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"} > 
-          <div
-            className="CROSS-ICON absolute top-0 right-0 px-8 "
-            onClick={() => setIsNavOpen(false)} 
-          >
-  
-          </div>
+        <div ref={mobileMenuRef} className={isNavOpen ? 'showMenuNav' : 'hideMenuNav'}>
+            <div className="CROSS-ICON absolute top-0 right-0 px-8 " onClick={() => setIsNavOpen(false)}>
+              {/* Your cross icon SVG */}
+            </div>
 
-          <div className="main flex">
+          <div className="main flex gap-1 ">
            <div className="left">
             <p> <span class="material-symbols-outlined">public</span> START HERE</p>
            <a href="/"> <div>
